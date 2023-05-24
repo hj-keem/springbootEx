0c6a6f4e6595bc347e59391520ac87b55b528259
김현정 <hyeomjeom@gmail.com>
2023-05-24 14:34:59 +0900

    feat: SimpleConnectionMaker로 분리

--- src/main/java/com/example/Springbootgradle/dao/UserDao.java
+++ src/main/java/com/example/Springbootgradle/dao/UserDao.java
@@ -6,20 +6,11 @@ import java.sql.*;
 import java.util.Map;
 
 import static java.lang.System.getenv;
-
 public abstract class UserDao {
-    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-//    {
-//        Map<String, String> env = getenv();
-//        String dbHost = env.get("DB_HOST");
-//        String dbUser = env.get("DB_USER");
-//        String dbPassword = env.get("DB_PASSWORD");
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
-//        return conn;
-//    }
+    SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
+
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
 
         PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values (? ,?,?)");
         pstmt.setString(1, user.getId());
@@ -32,7 +23,7 @@ public abstract class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection conn = getConnection();
+        Connection conn = connectionMaker.makeNewConnection();
 
         PreparedStatement pstmt = conn.prepareStatement("select id, name, password from users where id = ?");
         pstmt.setString(1, id);
@@ -54,15 +45,17 @@ public abstract class UserDao {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new NUserDao();
         User user = new User();
-        user.setId("2");
-        user.setName("kyeongrok2");
-        user.setPassword("5678");
+        user.setId("3");
+        user.setName("kyeongrok3");
+        user.setPassword("12345");
         userDao.add(user);
 
-        User selectedUser = userDao.get("2");
+        User selectedUser = userDao.get("3");
         System.out.println(selectedUser.getId());
         System.out.println(selectedUser.getName());
         System.out.println(selectedUser.getPassword());
     }
+
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 }
 
